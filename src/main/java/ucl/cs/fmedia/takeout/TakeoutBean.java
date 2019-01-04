7517c178ad9cb18ed08635edcbdf84a615866d75
David Guzman <d.guzman@ucl.ac.uk>
2019-01-04 18:57:33 +0000

    Add element check for totalsByDate in TakeoutBean

--- src/main/java/ucl/cs/fmedia/takeout/TakeoutBean.java
+++ src/main/java/ucl/cs/fmedia/takeout/TakeoutBean.java
@@ -1,16 +1,21 @@
 package ucl.cs.fmedia.takeout;
 
 import javax.ejb.Stateless;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
 
 /**
  * @author David Guzman {@literal d.guzman at ucl.ac.uk}
  */
 @Stateless
 public class TakeoutBean {
+
+  private final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
 
   @PersistenceContext
   private EntityManager entityManager;
@@ -24,6 +29,18 @@ public class TakeoutBean {
       .atZone(ZoneId.systemDefault())
       .toLocalDate();
     entity.setStartDate(localDate);
+    entityManager.persist(entity);
+  }
+
+  public void persistEntry(JsonObject entry) {
+    TakeoutEntity entity = new TakeoutEntity();
+    LocalDate startDate = LocalDate.parse(entry.getString("startDate"), formatter);
+    entity.setStartDate(startDate);
+    entity.setTotalQueries(entry.getInt("totalQueries"));
+    if (!entry.containsKey("totalsByDate")) {
+      throw new NoSuchElementException("totalsByDate JsonArray is missing");
+    }
+    entity.setTotalsByDate(entry.getJsonArray("totalsByDate"));
     entityManager.persist(entity);
   }
 
